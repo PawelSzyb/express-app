13730cf37a06b5ecb35bbcf03155faa74caa52d1
Paweł Szybaj <pawelszybaj@wp.pl>
2018-11-18 12:10:34 +0100

    First commit for app express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const rootDirectory = require("./utils/path");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDirectory, "views", "404.html"));
+});
+
+app.listen(3000, () => console.log(`Server started at port ${3000}`));
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const rootDirectory = require("../utils/path");
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(rootDirectory, "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
